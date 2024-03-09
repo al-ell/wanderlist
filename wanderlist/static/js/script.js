630670f76e35644f83b36c7f6439becdec26f9fa
Alice Dean <elliott.alice.92@gmail.com>
2024-03-09 16:51:37 +0000

    add navbar to base.html

---
+++ wanderlist/static/js/script.js
@@ -0,0 +1,6 @@
+// Materlialize JS: 
+document.addEventListener('DOMContentLoaded', function() {
+    // Mobile side menu
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
