92efe9ddd4661885b63ac94fde06718ac3be0305
Alice Dean <elliott.alice.92@gmail.com>
2024-03-10 18:57:40 +0000

    add new templates, rest countries api functional

--- wanderlist/routes.py
+++ wanderlist/routes.py
@@ -1,7 +1,9 @@
 import os
-from flask import render_template
+from flask import Flask, render_template
 from wanderlist import app, db
 from wanderlist.models import Itineraries, Journal
+import requests
+import json
 
 
 @app.route("/")
@@ -14,3 +16,10 @@ def home():
 def journal_entries():
     
     return render_template("journal_entries.html")
+
+
+@app.route("/destinations", methods=["GET"])
+def destinations():
+    req = requests.get("https://restcountries.com/v3.1/all")
+    data = json.loads(req.content)
+    return render_template("destinations.html", data=data)
