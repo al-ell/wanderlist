5fb5d19aac27dec368a20f2fded7b08b52d5e438
Alice Dean <elliott.alice.92@gmail.com>
2024-03-28 16:32:17 +0000

    update to JQuery for added form functionality

--- wanderlist/static/js/script.js
+++ wanderlist/static/js/script.js
@@ -1,50 +1,29 @@
-// Materlialize JS: 
-document.addEventListener('DOMContentLoaded', function() {
+// Materlialize JQuery: 
+$(document).ready(function(){
     // Mobile side menu
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+    $(".sidenav").sidenav({edge: "right"});
 
     // initialize dropdown
-    let dropdown_trigger = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(dropdown_trigger);
+    $('.dropdown-trigger').dropdown();
 
     // initialize datepicker
-    let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-        format: "dd mmm, yyyy",
-        i18n: {done: "Choose"}
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 15,
+        maxDate: "1",
+        showClearBtn: true,
+        i18n: {
+            done: "Choose"
+        }
     });
 
     // form select initialization
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+    $('select').formSelect();
 
     // initialize collapsible 
-    let collapsibles = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(collapsibles);
+    $('.collapsible').collapsible();
 
     // initialize modal
-    let modal = document.querySelectorAll('.modal');
-    M.Modal.init(modal);
-
-
-
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function() {stickyHeader()};
-
-    // Get the header
-    let header = document.getElementById("base-header");
-
-    // Get the offset position of the navbar
-    let sticky = header.offsetTop;
-
-    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function stickyHeader() {
-        if (window > sticky) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
-        }
-    }
+    $('.modal').modal();
 
 });
