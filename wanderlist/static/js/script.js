963b59982e49dce5fcec7ef09c75c58d3093030a
Alice Dean <elliott.alice.92@gmail.com>
2024-03-29 21:25:47 +0000

    JS linting complete: 14 warnings

--- wanderlist/static/js/script.js
+++ wanderlist/static/js/script.js
@@ -1,30 +1,29 @@
 // Materlialize JS: 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     // Mobile side menu
-    let sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // initialize dropdown
-    let dropdown_trigger = document.querySelectorAll('.dropdown-trigger');
+    let dropdown_trigger = document.querySelectorAll(".dropdown-trigger");
     M.Dropdown.init(dropdown_trigger);
 
     // initialize datepicker
-    let datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmm, yyyy",
         i18n: {done: "Choose"}
     });
 
     // form select initialization
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+    let formSelect = document.querySelectorAll("select");
+    M.formSelect.init(formSelect);
 
-    // initialize collapsible 
-    let collapsibles = document.querySelectorAll('.collapsible');
+    // initialize collapsible
+    let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
 
     // initialize modal
-    let modal = document.querySelectorAll('.modal');
+    let modal = document.querySelectorAll(".modal");
     M.Modal.init(modal);
-
 });
