2bfeb976d2de790a5b8ea8e92141f28e0c331c45
Alice Dean <elliott.alice.92@gmail.com>
2024-04-03 18:58:43 +0000

    bug fix: dissapearing date once selected

--- wanderlist/static/js/script.js
+++ wanderlist/static/js/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // initialize datepicker
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        format: "dd mm, yyyy",
+        format: "dd mmm, yyyy",
         maxDate: new Date(),
         i18n: {done: "Choose"}
     });
